399ce1855c93afd782f68c7b8c358d054b84caa4
arzailants <andrerizaldib@gmail.com>
2021-12-03 15:45:12 +0700

    Menambahkan Switch Statement

--- src/main/java/SwitchStatement.java
+++ src/main/java/SwitchStatement.java
@@ -21,5 +21,38 @@ public class SwitchStatement {
             default :
                 System.out.println("Not in range");
         }
+
+
+        System.out.println("===========================================");
+
+        int month = 11;
+        String season;
+
+        switch (month){
+            case 12 :
+            case 1 :
+            case 2 :
+                season = "Winter";
+                break;
+            case 3 :
+            case 4 :
+            case 5 :
+                season = "Spring";
+                break;
+            case 6 :
+            case 7 :
+            case 8 :
+                season = "Summer";
+                break;
+            case 9 :
+            case 10 :
+            case 11 :
+                season = "Autumn";
+                break;
+            default :
+                season = "Wrong Month";
+        }
+
+        System.out.println("Season saat ini : " + season);
     }
 }
