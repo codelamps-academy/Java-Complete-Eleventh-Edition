a13137636a9a485886d99d2fe726aede0a7cd03a
arzailants <andrerizaldib@gmail.com>
2021-12-15 15:47:39 +0700

    Menambahkan kelas

--- src/main/java/ObjectMethod.java
+++ src/main/java/ObjectMethod.java
@@ -5,11 +5,19 @@ public class ObjectMethod {
         Test myObjek1 = new Test(10);
         Test myObjek2;
 
+        System.out.println("myObjek1.a = " +myObjek1.a);
+//        System.out.println("myObjek2.a = " +myObjek2.a);
+
+        myObjek1 = myObjek1.incrByTen();
         myObjek2 = myObjek1.incrByTen();
-        myObjek1.incrByTen();
+
 
         System.out.println("myObjek1.a = " +myObjek1.a);
+        System.out.println("myObjek2.a = " +myObjek2.a);
 
+        myObjek1 = myObjek1.incrByTen();
+        myObjek2 = myObjek2.incrByTen();
+        System.out.println("myObjek1.a = " +myObjek1.a);
         System.out.println("myObjek2.a = " +myObjek2.a);
 
     }
